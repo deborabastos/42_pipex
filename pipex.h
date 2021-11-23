3b95a4f342959120debedbf26b5971c38022bccd
deborabastos <deborabastos14@gmail.com>
2021-11-23 16:42:59 -0300

    included headears

--- pipex.h
+++ pipex.h
@@ -6,7 +6,19 @@
 /*   By: dalves-p <dalves-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 16:29:46 by dalves-p          #+#    #+#             */
-/*   Updated: 2021/11/23 16:29:47 by dalves-p         ###   ########.fr       */
+/*   Updated: 2021/11/23 16:42:40 by dalves-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <unistd.h> //access, unlink, close, read, write, pipe, dup, dup2, execve, fork
+# include <fcntl.h> // open
+# include <stdlib.h> // malloc, free, exit
+# include <sys/wait.h> // wait, waitpid
+# include <errno.h> // perror
+# include <string.h> // strerror
+
+
+#endif
