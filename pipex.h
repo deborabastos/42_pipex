1d402c83e0199f40f2ca1995eedeeee271b2071c
deborabastos <deborabastos14@gmail.com>
2021-11-24 20:53:30 -0300

    README

--- pipex.h
+++ pipex.h
@@ -6,14 +6,14 @@
 /*   By: dalves-p <dalves-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 16:29:46 by dalves-p          #+#    #+#             */
-/*   Updated: 2021/11/23 16:44:54 by dalves-p         ###   ########.fr       */
+/*   Updated: 2021/11/24 16:11:18 by dalves-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-# include <unistd.h> //access, unlink, close, read, write, pipe, dup, dup2, execve, fork
+# include <unistd.h> //access,unlink,close,read,write,pipe,dup,dup2,execve,fork
 # include <fcntl.h> // open
 # include <stdlib.h> // malloc, free, exit
 # include <sys/wait.h> // wait, waitpid
